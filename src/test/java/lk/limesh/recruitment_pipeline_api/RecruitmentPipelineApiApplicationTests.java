9aa18ddc32b5694a39eef7b18ff018040c119d76
Limesh-B <limesh075@gmail.com>
2025-05-16 20:21:31 +0530

    Initial commit

---
+++ src/test/java/lk/limesh/recruitment_pipeline_api/RecruitmentPipelineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.limesh.recruitment_pipeline_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentPipelineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
