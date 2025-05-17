3c4800f698ab5701b1dd86dc473c668d69222d75
Limesh-B <limesh075@gmail.com>
2025-05-17 14:51:58 +0530

    Updated database configuration

--- src/main/java/lk/limesh/recruitment_pipeline_api/RecruitmentPipelineApiApplication.java
+++ src/main/java/lk/limesh/recruitment_pipeline_api/RecruitmentPipelineApiApplication.java
@@ -2,11 +2,18 @@ package lk.limesh.recruitment_pipeline_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class RecruitmentPipelineApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Loads .env file
+
+		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
 
 		SpringApplication.run(RecruitmentPipelineApiApplication.class, args);
 	}
